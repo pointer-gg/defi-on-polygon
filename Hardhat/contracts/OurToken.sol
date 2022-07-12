b58d9d751b098c06e4504e6479ab518b4329a3e6
vincanger <70215737+vincanger@users.noreply.github.com>
2022-07-12 15:00:50 +0200

    add hardhat, token contract, and test
    Chapter 2. > Designing the Forum Backend
    
    add hardhat, token contract, and test
    
    Chapter 2 > Our ERC20 Token > Testing

---
+++ Hardhat/contracts/OurToken.sol
@@ -0,0 +1,58 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Goflow is IERC20 {
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+    string public name = "Goflow";
+    string public symbol = "GOFLOW";
+    uint256 public decimals = 18;
+
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool)
+    {
+        balanceOf[msg.sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        require(msg.sender != spender, "Cannot approve self");
+        require(
+            balanceOf[msg.sender] >= amount,
+            "insufficient balance for approval!"
+        );
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool) {
+        allowance[sender][msg.sender] -= amount;
+        balanceOf[sender] -= amount;
+        balanceOf[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+
+    function mint(uint256 amount) external {
+        balanceOf[msg.sender] += amount;
+        totalSupply += amount;
+        emit Transfer(address(0), msg.sender, amount);
+    }
+
+    function burn(uint256 amount) external {
+        balanceOf[msg.sender] -= amount;
+        totalSupply -= amount;
+        emit Transfer(msg.sender, address(0), amount);
+    }
+}
---
+++ Hardhat/test/OurToken.test.ts
@@ -0,0 +1,81 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { ethers } from "hardhat";
+
+describe("Goflow", function () {
+  let goflow: Contract, owner: SignerWithAddress, otherAccount:SignerWithAddress;
+
+  const deployContract = async () => {
+    
+    // Contracts are deployed using the first signer/account by default
+    const [_owner, _otherAccount] = await ethers.getSigners();
+
+    const Goflow = await ethers.getContractFactory("Goflow");
+    goflow = await Goflow.deploy();
+
+    owner = _owner;
+    otherAccount = _otherAccount; 
+  }
+
+  const mint = async (user: SignerWithAddress, amount: number) => {
+    const tx = await goflow.connect(user).mint(amount);
+    await tx.wait();
+  }
+
+  beforeEach( async () => {
+    await deployContract();
+  })
+
+  describe("Deployment", () => {
+    it("Should deploy and return correct symbol", async () => {
+      expect(await goflow.symbol()).to.equal("GOFLOW");
+    });
+  });
+
+  describe("Minting", () => {
+    it("Should mint tokens to user", async () => {
+      await mint(owner, 100);
+      expect(await goflow.balanceOf(owner.address)).to.equal(100);
+    });
+  });
+
+  describe("Transfer", () => {
+    it("Should transfer tokens to another account", async () => {
+      await mint(owner, 100); // mint 100 tokens to owner
+
+      await goflow.transfer(otherAccount.address, 100);
+      expect(await goflow.balanceOf(owner.address)).to.equal(0);
+      expect(await goflow.balanceOf(otherAccount.address)).to.equal(100);
+    });
+  });
+
+  describe("TransferFrom", () => {
+    it("Should approve a spender to be able to transfer owner's tokens", async () => {
+      await mint(owner, 100); // owner should start with 100 tokens
+      expect(await goflow.balanceOf(owner.address)).to.equal(100);
+
+      // we have to approve the "spender" to access the owner's tokens
+      const approve = await goflow.approve(otherAccount.address, 100);
+      await approve.wait();
+      // use the connect method to connect to the otherAccount
+      const transferFrom = await goflow.connect(otherAccount).transferFrom(owner.address, otherAccount.address, 100);
+      await transferFrom.wait();
+      // the spender should now have transferred the 100 tokens to themselves
+      expect(await goflow.balanceOf(owner.address)).to.equal(0);
+      expect(await goflow.balanceOf(otherAccount.address)).to.equal(100);
+    });
+
+    it("Should not allow a spender to transfer more tokens than they have", async () => {
+      await mint(owner, 100); // owner should start with 100 tokens
+      expect(await goflow.balanceOf(owner.address)).to.equal(100);
+
+      // the approve method should throw an error 
+      // if the user tries to approve more tokens than they have
+      // NOTE: move `await` to the beginning of the expect statement
+      // NOTE: the error message comes from our approve method's `require` statement
+      await expect(goflow.approve(otherAccount.address, 200)).to.be.revertedWith('insufficient balance for approval!');
+    });
+  });
+
+});
