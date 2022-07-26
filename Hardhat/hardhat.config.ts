7d95d86d0f648a7bc5f19c10a3bccccd9c8359a9
vincanger <70215737+vincanger@users.noreply.github.com>
2022-07-26 19:58:47 +0200

    add RPC Provider & Mumbai Deploy config
    end state of Chapter 3. > Deployment & Final Words > Deploy to Polygon

--- Hardhat/hardhat.config.ts
+++ Hardhat/hardhat.config.ts
@@ -1,11 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import 'dotenv/config';
 
 const config: HardhatUserConfig = {
   solidity: '0.8.9',
   networks: {
     hardhat: {
       chainId: 1337,
+    },
+    matic: {
+      url: '<YOUR-QUICKNODE-POLYGON-URL>',
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
     },
   },
 };
